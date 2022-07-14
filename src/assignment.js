a5f74bf443811e993b26d24102dd0a6133b88e62
Timilehin Ogundele <52321812+timiblaze@users.noreply.github.com>
2022-07-14 21:18:15 +0000

    assignment 1 and 2

--- src/assignment.js
+++ src/assignment.js
@@ -13,10 +13,18 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
+
 function sumOfNumbers(arrayOfNumbers) {
+    let sum = 0;
+  for (let i = 0; i < arrayOfNumbers.length; i++)
+    {
+    sum += arrayOfNumbers[i]
+    }
+  return (sum);
     return arrayOfNumbers.length;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +36,21 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
+    let length = arrayOfNumbers.length;
+    let i = 0;
+    let countOfEven = 0;
+    while (i < length) {
+      if (arrayOfNumbers[i] % 2 == 0) {
+        countOfEven++;
+        
+      }
+      i++;
+      
+    }
+    return (countOfEven);
     return arrayOfNumbers.length;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
